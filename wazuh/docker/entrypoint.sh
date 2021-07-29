851d02bf575e6cd88f74407f768d2ad83e9765a8
Patrick Ethier <nuttybrewer@gmail.com>
2021-07-29 15:27:07 +0200

    Add filebeat components

--- wazuh/docker/entrypoint.sh
+++ wazuh/docker/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 service wazuh-manager start
+service filebeat start
+
+# Check that wazuh manager started properly
+/var/ossec/bin/cluster_control -l
+# Check that filebeat started properly
+# filebeat test output
 tail -F /var/ossec/logs/archives/archives.json
