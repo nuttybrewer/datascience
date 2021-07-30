80732fb764d8f11b5cee35ed9b3004921b1fdc4b
Patrick Ethier <nuttybrewer@gmail.com>
2021-07-30 22:28:44 +0200

    Missing space

--- wazuh/docker/entrypoint.sh
+++ wazuh/docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 service wazuh-manager start
 # Create self-sign certs if they don't exist for filebeat
-if [[ ! -f "/etc/filebeat/certs/filebeat.pem"]]; then
-  if [[ ! -d '/etc/filebeat/certs' ]]; then
+if [[ ! -f "/etc/filebeat/certs/filebeat.pem" ]]; then
+  if [[ ! -d "/etc/filebeat/certs" ]]; then
     mkdir /etc/filebeat/certs
   fi
   openssl req -nodes -x509 -newkey rsa:4096 -keyout /etc/filebeat/certs/filebeat-key.pem -out /etc/filebeat/certs/filebeat.pem -days 365 -subj "/CN=myfilebeatnode.internal/emailAddress=dev@example.internal"
