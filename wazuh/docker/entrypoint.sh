3037a384e405f98f526ad6692e65928dafbb21eb
Patrick Ethier <nuttybrewer@gmail.com>
2021-07-31 17:00:58 +0200

    The file is empty for some reason

--- wazuh/docker/entrypoint.sh
+++ wazuh/docker/entrypoint.sh
@@ -22,7 +22,8 @@ if [[ $WAZUH_CLUSTER_DISABLED -eq "no" ]]; then
   fi
 
   # Output config file, strip the root elements first!
-  echo $XML_CONFIG | tail -n +2 | head -n -1 > /var/ossec/etc/ossec.conf
+  echo $XML_CONFIG
+  echo $XML_CONFIG | tail -n +2 | head -n "-1"
 
   # Load up the config for the next batch of editing
   XML_CONFIG=$(echo "<root>$(cat /var/ossec/etc/ossec.conf)</root>")
