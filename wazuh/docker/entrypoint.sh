823c08ec7e1785ac623015af7dd522e2570cd987
Patrick Ethier <nuttybrewer@gmail.com>
2021-07-31 18:53:22 +0200

    Filebeat configuration via yq

--- wazuh/docker/entrypoint.sh
+++ wazuh/docker/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-echo "${WAZUH_CLUSTER_DISABLED}"
-echo "${WAZUH_CLUSTER_NODE_TYPE}"
-echo "${WAZUH_CLUSTER_MASTER}"
+echo "Provided environment variables"
+echo "WAZUH_CLUSTER_DISABLED: ${WAZUH_CLUSTER_DISABLED:-yes}"
+echo "WAZUH_CLUSTER_NODE_TYPE: ${WAZUH_CLUSTER_NODE_TYPE:-worker}"
+echo "WAZUH_CLUSTER_MASTER: ${WAZUH_CLUSTER_MASTER}"
+echo "FILEBEAT_ES_HOSTS: ${FILEBEAT_ES_HOSTS}"
+echo "FILEBEAT_ES_USER: ${FILEBEAT_ES_USER}"
+
 # echo "${WAZUH_CLUSTER_KEY}"
-echo "${WAZUH_CLUSTER_NAME}"
+echo "WAZUH_CLUSTER_MASTER: ${WAZUH_CLUSTER_NAME:-wazuh}"
 
 # The configuration file isn't valid XML, we need to wrap it in root tags
 XML_CONFIG=$(echo "<root>$(cat /var/ossec/etc/ossec.conf)</root>")
@@ -29,6 +33,7 @@ if [[ $WAZUH_CLUSTER_DISABLED -eq "no" ]]; then
 fi
 
 service wazuh-manager start
+
 # Create self-sign certs if they don't exist for filebeat
 if [[ ! -f "/etc/filebeat/certs/filebeat.pem" ]]; then
   if [[ ! -d "/etc/filebeat/certs" ]]; then
@@ -37,6 +42,16 @@ if [[ ! -f "/etc/filebeat/certs/filebeat.pem" ]]; then
   openssl req -nodes -x509 -newkey rsa:4096 -keyout /etc/filebeat/certs/filebeat-key.pem -out /etc/filebeat/certs/filebeat.pem -days 365 -subj "/CN=myfilebeatnode.internal/emailAddress=dev@example.internal"
   cp /etc/filebeat/certs/filebeat.pem /etc/filebeat/certs/root-ca.pem
   chmod 0600 /etc/filebeat/certs/*.pem
+fi
+
+# Configure filebeats with environment variables
+if [[ $FILEBEAT_ES_HOSTS ]]; then
+  yq eval '.["output.elasticsearch"].hosts=env(FILEBEAT_ES_HOSTS) | .["output.elasticsearch"].hosts[] style="double"' /etc/filebeat/filebeat.yml > /etc/filebeat/filebeat.yml
+  if [[ $FILEBEAT_ES_USER ]]; then
+    yq eval '.["output.elasticsearch"].username = env(FILEBEAT_ES_USER) | .["output.elasticsearch"].password=env(FILEBEAT_ES_PASS)' /etc/filebeat/filebeat.yml > /etc/filebeat/filebeat.yml
+  else
+    yq eval 'del(.["output.elasticsearch"].username) | del(.["output.elasticsearch"].password)' /etc/filebeat/filebeat.yml > /etc/filebeat/filebeat.yml
+  fi
 fi
 service filebeat start
 
