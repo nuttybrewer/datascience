c1218a375f234058f52217c7133108bd69007411
Patrick Ethier <nuttybrewer@gmail.com>
2021-08-01 22:52:00 +0200

    Changed certificate creation logic around to use directory detection

--- wazuh/docker/entrypoint.sh
+++ wazuh/docker/entrypoint.sh
@@ -35,10 +35,9 @@ fi
 service wazuh-manager start
 
 # Create self-sign certs if they don't exist for filebeat
-if [[ ! -f "/etc/filebeat/certs/filebeat.pem" ]]; then
-  if [[ ! -d "/etc/filebeat/certs" ]]; then
-    mkdir /etc/filebeat/certs
-  fi
+if [[ ! -d "/etc/filebeat/certs" ]]; then
+  echo "Creating self-signed certs for Filebeat"
+  mkdir /etc/filebeat/certs
   openssl req -nodes -x509 -newkey rsa:4096 -keyout /etc/filebeat/certs/filebeat-key.pem -out /etc/filebeat/certs/filebeat.pem -days 365 -subj "/CN=myfilebeatnode.internal/emailAddress=dev@example.internal"
   cp /etc/filebeat/certs/filebeat.pem /etc/filebeat/certs/root-ca.pem
   chmod 0600 /etc/filebeat/certs/*.pem
