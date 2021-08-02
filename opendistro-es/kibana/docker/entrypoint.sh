a62614767cd348115013da2ed0d00352fdfca519
Patrick Ethier <nuttybrewer@gmail.com>
2021-08-02 16:55:05 +0200

    Comment out to see where the script crashes

--- opendistro-es/kibana/docker/entrypoint.sh
+++ opendistro-es/kibana/docker/entrypoint.sh
@@ -1,12 +1,12 @@
-
+#!/bin/bash
 umask 0002
 
-if [[ -z "${KIBANA_PLUGINS_SPACE_DELIMITED}" ]]; then
-  IFS=' ' read -r -a PLUGINS <<< "${KIBANA_PLUGINS_SPACE_DELIMITED}"
-  for PLUGIN in "${PLUGINS[@]}";do
-    echo "Installing ${PLUGIN}"
-    exec /usr/share/kibana/bin/kibana-plugin install "${PLUGIN}"
-  done
-fi
+# if [[ -z "${KIBANA_PLUGINS_SPACE_DELIMITED}" ]]; then
+#   IFS=' ' read -r -a PLUGINS <<< "${KIBANA_PLUGINS_SPACE_DELIMITED}"
+#   for PLUGIN in "${PLUGINS[@]}";do
+#     echo "Installing ${PLUGIN}"
+#     exec /usr/share/kibana/bin/kibana-plugin install "${PLUGIN}"
+#   done
+# fi
 
-# /usr/local/bin/kibana-docker
+/usr/local/bin/kibana-docker
