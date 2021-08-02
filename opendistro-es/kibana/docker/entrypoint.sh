61b1d420c199aac0c839a6f874e083fd6791f077
Patrick Ethier <nuttybrewer@gmail.com>
2021-08-02 17:05:59 +0200

    Remove the array stuff for troubleshooting

--- opendistro-es/kibana/docker/entrypoint.sh
+++ opendistro-es/kibana/docker/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 umask 0002
-
+echo "KIBANA_PLUGINS_SPACE_DELIMITED: ${KIBANA_PLUGINS_SPACE_DELIMITED:-None}"
 if [[ -z "${KIBANA_PLUGINS_SPACE_DELIMITED}" ]]; then
-  IFS=' ' read -r -a PLUGINS <<< "${KIBANA_PLUGINS_SPACE_DELIMITED}"
-  for PLUGIN in "${PLUGINS[@]}";do
-    echo "Installing ${PLUGIN}"
-    exec /usr/share/kibana/bin/kibana-plugin install "${PLUGIN}"
-  done
+  echo "Installing external kibana plugins..."
+  # IFS=' ' read -r -a PLUGINS <<< "${KIBANA_PLUGINS_SPACE_DELIMITED}"
+  # for PLUGIN in "${PLUGINS[@]}";do
+  #   echo "Installing ${PLUGIN}"
+  #   exec /usr/share/kibana/bin/kibana-plugin install "${PLUGIN}"
+  # done
 fi
 
 /usr/local/bin/kibana-docker
