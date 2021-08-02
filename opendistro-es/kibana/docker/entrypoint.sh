cf23bc0fc822f0ab6ddbbf865d3bb2461535cc7a
Patrick Ethier <nuttybrewer@gmail.com>
2021-08-02 22:37:42 +0200

    Fix bad if/else

--- opendistro-es/kibana/docker/entrypoint.sh
+++ opendistro-es/kibana/docker/entrypoint.sh
@@ -13,15 +13,15 @@ fi
 
 # Copy over know plugin configurations
 if [[ -f "/var/run/wazuh.yml" ]]; then
+  echo "Checking if wazuh config directory exists"
   if [[ ! -d "/usr/share/kibana/data/wazuh/config" ]]; then
     echo "Creating config directory /usr/share/kibana/data/wazuh/config"
     mkdir -p "/usr/share/kibana/data/wazuh/config"
   fi
-    echo "Detected wazuh plugin confuration, copying into place"
-    cp /var/run/wazuh.yml /usr/share/kibana/data/wazuh/config/wazuh.yml
-  else
-    echo "Unable to copy over configuration, Wazuh plugin likely not installed correctly"
-  fi
+  echo "Detected wazuh plugin confuration, copying into place"
+  cp /var/run/wazuh.yml /usr/share/kibana/data/wazuh/config/wazuh.yml
+else
+  echo "Unable to copy over configuration, Wazuh plugin likely not installed correctly"
 fi
 
 # Run the original command file from amazon/ODFE-kibana docker image
