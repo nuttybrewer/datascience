c59d67a7eaa813d775114b4bc68838283fbb27ff
Patrick Ethier <nuttybrewer@gmail.com>
2021-08-02 22:40:22 +0200

    Remove exec statement

--- opendistro-es/kibana/docker/entrypoint.sh
+++ opendistro-es/kibana/docker/entrypoint.sh
@@ -7,7 +7,7 @@ if [[ ! -z "${KIBANA_PLUGINS_SPACE_DELIMITED}" ]]; then
   IFS=' ' read -r -a PLUGINS <<< "${KIBANA_PLUGINS_SPACE_DELIMITED}"
   for PLUGIN in "${PLUGINS[@]}";do
     echo "Installing ${PLUGIN}"
-    exec /usr/share/kibana/bin/kibana-plugin install "${PLUGIN}"
+    /usr/share/kibana/bin/kibana-plugin install "${PLUGIN}"
   done
 fi
 
