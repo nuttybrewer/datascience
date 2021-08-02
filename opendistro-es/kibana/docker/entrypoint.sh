4085b2f46a1195b1df883bc08df82793f262812e
Patrick Ethier <nuttybrewer@gmail.com>
2021-08-02 17:58:21 +0200

    Add just the text versions

--- opendistro-es/kibana/docker/entrypoint.sh
+++ opendistro-es/kibana/docker/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "KIBANA_PLUGINS_SPACE_DELIMITED: ${KIBANA_PLUGINS_SPACE_DELIMITED:-None}"
-# if [[ -z "${KIBANA_PLUGINS_SPACE_DELIMITED}" ]]; then
-#  echo "Installing external kibana plugins..."
-  # IFS=' ' read -r -a PLUGINS <<< "${KIBANA_PLUGINS_SPACE_DELIMITED}"
-  # for PLUGIN in "${PLUGINS[@]}";do
-  #   echo "Installing ${PLUGIN}"
+if [[ ! -z "${KIBANA_PLUGINS_SPACE_DELIMITED}" ]]; then
+  echo "Installing external kibana plugins..."
+  IFS=' ' read -r -a PLUGINS <<< "${KIBANA_PLUGINS_SPACE_DELIMITED}"
+  for PLUGIN in "${PLUGINS[@]}";do
+    echo "Installing ${PLUGIN}"
   #   exec /usr/share/kibana/bin/kibana-plugin install "${PLUGIN}"
-  # done
-# fi
+  done
+fi
 
 # /usr/local/bin/kibana-docker
