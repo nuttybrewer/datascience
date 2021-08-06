902eabd23c0f345e26dea9cac7460d64e51e8f6c
Patrick Ethier <nuttybrewer@gmail.com>
2021-08-06 14:42:54 +0200

    Fix script to use subnodes instead of insert on xmlstarlet

--- wazuh/docker/entrypoint-agent.sh
+++ wazuh/docker/entrypoint-agent.sh
@@ -17,25 +17,23 @@ if [[ $WAZUH_CONFIG_USE_MOUNTED_VOLUME != "yes" ]]; then
 
   if [[ $WAZUH_AGENT_DISABLED == "no" ]]; then
     echo "Configuring wazuh agent"
-    XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config" -t elem -n client)
-    XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client" -t elem -n server)
-    XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/server" -t elem -n address -v "${WAZUH_AGENT_SERVICE_ADDRESS}")
+    XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -u "/root/ossec_config/client/server/address" -v "${WAZUH_AGENT_SERVICE_ADDRESS}")
     if [[ $WAZUH_AGENT_ENROLLMENT_DISABLED == "no" ]]; then
-      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client" -t elem -n enrollment)
-      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "enabled" -t elem -v "yes")
-      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "agent_name" -t elem -v "${WAZUH_AGENT_ENROLLMENT_AGENT_NAME:-$(hostname)}")
-      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "manager_address" -t elem -v "${WAZUH_AGENT_ENROLLMENT_MANAGER_ADDRESS}")
+      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -s "/root/ossec_config/client" -t elem -n "enrollment")
+      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -s "/root/ossec_config/client/enrollment" -t elem -n "enabled"  -v "yes")
+      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -s "/root/ossec_config/client/enrollment" -t elem -n "agent_name" -v "${WAZUH_AGENT_ENROLLMENT_AGENT_NAME:-$(hostname)}")
+      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -s "/root/ossec_config/client/enrollment" -t elem -n "manager_address" -v "${WAZUH_AGENT_ENROLLMENT_MANAGER_ADDRESS}")
       if [[ ! -z $WAZUH_AGENT_ENROLLMENT_CA_PATH ]]; then
-        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "server_ca_path" -t elem -v "${WAZUH_AGENT_ENROLLMENT_CA_PATH}")
+        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -s "/root/ossec_config/client/enrollment" -t elem -n "server_ca_path" -v "${WAZUH_AGENT_ENROLLMENT_CA_PATH}")
       fi
       if [[ ! -z $WAZUH_AGENT_ENROLLMENT_KEY_PATH ]]; then
-        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "agent_key_path" -t elem -v "${WAZUH_AGENT_ENROLLMENT_KEY_PATH}")
+        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -s "/root/ossec_config/client/enrollment" -t elem -n "agent_key_path" -v "${WAZUH_AGENT_ENROLLMENT_KEY_PATH}")
       fi
       if [[ ! -z $WAZUH_AGENT_ENROLLMENT_CERT_PATH ]]; then
-        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "agent_certification_path" -t elem -v "${WAZUH_AGENT_ENROLLMENT_CERT_PATH}")
+        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -s "/root/ossec_config/client/enrollment" -t elem -n "agent_certification_path" -v "${WAZUH_AGENT_ENROLLMENT_CERT_PATH}")
       fi
       if [[ ! -z $WAZUH_AGENT_ENROLLMENT_CRED_PATH ]]; then
-        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "authorization_pass_path" -t elem -v "${WAZUH_AGENT_ENROLLMENT_CRED_PATH}")
+        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -s "/root/ossec_config/client/enrollment" -t elem -n "authorization_pass_path" -v "${WAZUH_AGENT_ENROLLMENT_CRED_PATH}")
       fi
     fi
   fi
@@ -43,6 +41,6 @@ if [[ $WAZUH_CONFIG_USE_MOUNTED_VOLUME != "yes" ]]; then
   XML_CONFIG=$(echo "<root>$(cat /var/ossec/etc/ossec.conf)</root>")
 fi
 
-service wazuh-manager start
+service wazuh-agent start
 
 tail -F /var/ossec/logs/ossec.log
