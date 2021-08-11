04e6db9e5f7d634151a7d35fd8666be0e13e9a49
Patrick Ethier <nuttybrewer@gmail.com>
2021-08-11 11:53:03 +0200

    Fix variable to use authd passphrase file to be consistent with the others

--- wazuh/docker/entrypoint.sh
+++ wazuh/docker/entrypoint.sh
@@ -6,18 +6,9 @@ echo "WAZUH_CLUSTER_NAME: ${WAZUH_CLUSTER_NAME:-wazuh}"
 echo "WAZUH_CLUSTER_NODE_TYPE: ${WAZUH_CLUSTER_NODE_TYPE:-worker}"
 echo "WAZUH_CLUSTER_MANAGER: ${WAZUH_CLUSTER_MANAGER:-localhost}"
 echo "WAZUH_AUTHD_DISABLED: ${WAZUH_AUTHD_DISABLED:-yes}"
+echo "WAZUH_AUTHD_AGENT_PASSPHRASE_DISABLED: ${WAZUH_AUTHD_AGENT_PASSPHRASE_DISABLED:-yes}"
 echo "WAZUH_AUTHD_AGENT_CA_DISABLED: ${WAZUH_AUTHD_AGENT_CA_DISABLED:-yes}"
 echo "WAZUH_AUTHD_AGENT_CA_PATH: ${WAZUH_AUTHD_AGENT_CA_PATH:-/var/ossec/etc/rootCA.pem}"
-echo "WAZUH_AUTHD_USE_PASSWORD: ${WAZUH_AUTH_USE_PASSWORD:-no}"
-echo "WAZUH_AGENT_DISABLED: ${WAZUH_AGENT_DISABLED:-yes}"
-echo "WAZUH_AGENT_SERVICE_ADDRESS: ${WAZUH_AGENT_SERVICE_ADDRESS:-None}"
-echo "WAZUH_AGENT_ENROLLMENT_DISABLED: ${WAZUH_AGENT_ENROLLMENT_DISABLED:-yes}"
-echo "WAZUH_AGENT_ENROLLMENT_AGENT_NAME: ${WAZUH_AGENT_ENROLLMENT_AGENT_NAME:-$(hostname)}"
-echo "WAZUH_AGENT_ENROLLMENT_MANAGER_ADDRESS: ${WAZUH_AGENT_ENROLLMENT_MANAGER_ADDRESS:-None}"
-echo "WAZUH_AGENT_ENROLLMENT_CA_PATH: ${WAZUH_AGENT_ENROLLMENT_CA_PATH:-None}"
-echo "WAZUH_AGENT_ENROLLMENT_KEY_PATH: ${WAZUH_AGENT_ENROLLMENT_KEY_PATH:-None}"
-echo "WAZUH_AGENT_ENROLLMENT_CERT_PATH: ${WAZUH_AGENT_ENROLLMENT_CERT_PATH:-None}"
-echo "WAZUH_AGENT_ENROLLMENT_CRED_PATH: ${WAZUH_AGENT_ENROLLMENT_CRED_PATH:-/etc/authd.pass}"
 echo "FILEBEAT_CONFIG_USE_MOUNTED_VOLUME: ${FILEBEAT_CONFIG_USE_MOUNTED_VOLUME:-no}"
 echo "FILEBEAT_ES_HOSTS: ${FILEBEAT_ES_HOSTS}"
 echo "FILEBEAT_ES_SSL_VERIFICATION_MODE: ${FILEBEAT_ES_SSL_VERIFICATION_MODE:-certificate}"
@@ -49,7 +40,9 @@ if [[ $WAZUH_CONFIG_USE_MOUNTED_VOLUME != "yes" ]]; then
   if [[ $WAZUH_AUTHD_DISABLED == "no" ]]; then
     echo "Turning on authd"
     XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -u "/root/ossec_config/auth/disabled" -v "no")
-    XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -u "/root/ossec_config/auth/use_password" -v "${WAZUH_AUTH_USE_PASSWORD:-no}")
+    if [[ $WAZUH_AUTHD_AGENT_PASSPHRASE_DISABLED == "no" ]];then
+      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -u "/root/ossec_config/auth/use_password" -v "yes")
+    fi
     if [[ $WAZUH_AUTHD_AGENT_CA_DISABLED == "no" ]]; then
       echo "Turning on ssl_agent_ca check for authd"
       XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/auth" -t elem -n "ssl_agent_ca" -v "${WAZUH_AUTHD_AGENT_CA_PATH:-/var/ossec/etc/rootCA.pem}")
@@ -58,30 +51,6 @@ if [[ $WAZUH_CONFIG_USE_MOUNTED_VOLUME != "yes" ]]; then
     XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -u "/root/ossec_config/cluster/disabled" -v "no")
   fi
 
-  if [[ $WAZUH_AGENT_DISABLED == "no" ]]; then
-    echo "Configuring wazuh agent"
-    XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config" -t elem -n client)
-    XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client" -t elem -n server)
-    XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/server" -t elem -n address -v "${WAZUH_AGENT_SERVICE_ADDRESS}")
-    if [[ $WAZUH_AGENT_ENROLLMENT_DISABLED == "no" ]]; then
-      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client" -t elem -n enrollment)
-      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "enabled" -t elem -v "yes")
-      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "agent_name" -t elem -v "${WAZUH_AGENT_ENROLLMENT_AGENT_NAME:-$(hostname)}")
-      XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "manager_address" -t elem -v "${WAZUH_AGENT_ENROLLMENT_MANAGER_ADDRESS}")
-      if [[ ! -z $WAZUH_AGENT_ENROLLMENT_CA_PATH ]]; then
-        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "server_ca_path" -t elem -v "${WAZUH_AGENT_ENROLLMENT_CA_PATH}")
-      fi
-      if [[ ! -z $WAZUH_AGENT_ENROLLMENT_KEY_PATH ]]; then
-        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "agent_key_path" -t elem -v "${WAZUH_AGENT_ENROLLMENT_KEY_PATH}")
-      fi
-      if [[ ! -z $WAZUH_AGENT_ENROLLMENT_CERT_PATH ]]; then
-        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "agent_certification_path" -t elem -v "${WAZUH_AGENT_ENROLLMENT_CERT_PATH}")
-      fi
-      if [[ ! -z $WAZUH_AGENT_ENROLLMENT_CRED_PATH ]]; then
-        XML_CONFIG=$(echo $XML_CONFIG | xmlstarlet ed -O -i "/root/ossec_config/client/enrollment" -n "authorization_pass_path" -t elem -v "${WAZUH_AGENT_ENROLLMENT_CRED_PATH}")
-      fi
-    fi
-  fi
   echo "${XML_CONFIG}" | xmlstarlet fo -o | tail -n +2 | head -n "-1" > /var/ossec/etc/ossec.conf
   XML_CONFIG=$(echo "<root>$(cat /var/ossec/etc/ossec.conf)</root>")
 fi
