f29a1097bb4ea6b93f24b96d057b5249c5bfe0ba
Patrick Ethier <nuttybrewer@gmail.com>
2021-08-18 16:21:09 +0200

    bash->ash attempt #1

--- elastalert/docker/entrypoint.sh
+++ elastalert/docker/entrypoint.sh
@@ -31,11 +31,11 @@ if [[ ! -e "/opt/elastalert-server/initialized" ]]; then
   if [[ $ELASTALERT_CONFIG_USE_MOUNTED_VOLUME != "yes" ]]; then
     # The configuration file isn't valid XML, we need to wrap it in root tags
     ELASTALERT_CONFIG_JSON=$(cat /opt/elastalert-server/config/config.json)
-    if [[ $ELASTALERT_ES_CLIENT_TLS_ENABLED == "yes"]]; then
+    if [[ $ELASTALERT_ES_CLIENT_TLS_ENABLED = "yes"]]; then
       echo "Turning on TLS for client ES communications"
       ELASTALERT_CONFIG_JSON=$(echo $ELASTALERT_CONFIG_JSON | jq '.es_ssl = true')
       yq w -i /opt/elastalert-server/config/elastalert.yaml "use_ssl" "True"
-      if [[ $ELASTALERT_ES_CLIENT_VERIFY_CA == "yes" ]]; then
+      if [[ $ELASTALERT_ES_CLIENT_VERIFY_CA = "yes" ]]; then
         echo "Turning on ES service TLS validation"
         yq w -i /opt/elastalert-server/config/elastalert.yaml "verify_certs" "True"
       else
