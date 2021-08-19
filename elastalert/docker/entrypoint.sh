0bfb197473b1f3c344a647b12bd2684c96ef57f6
Patrick Ethier <nuttybrewer@gmail.com>
2021-08-19 12:25:02 +0200

    Re-enable Node.

--- elastalert/docker/entrypoint.sh
+++ elastalert/docker/entrypoint.sh
@@ -75,10 +75,8 @@ else
   echo "This container has previously been initialized. To cause it to reset, please delete the file /var/ossec/etc/initialized"
 fi
 
-# if [[ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]]; then
-#   set -- node "$@"
-# fi
+if [[ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]]; then
+  set -- node "$@"
+fi
 
-# exec "$@"
-
-tail -F /etc/ca-certificates.conf
+exec "$@"
