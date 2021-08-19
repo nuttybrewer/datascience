20ed95b869ff304b0f5a09b2833416f4ef116100
Patrick Ethier <nuttybrewer@gmail.com>
2021-08-19 13:26:50 +0200

    Configure elastalert too.

--- elastalert/docker/entrypoint.sh
+++ elastalert/docker/entrypoint.sh
@@ -38,37 +38,45 @@ if [[ ! -e "/opt/elastalert-server/initialized" ]]; then
       echo "Turning on TLS for client ES communications"
       ELASTALERT_CONFIG_JSON=$(echo $ELASTALERT_CONFIG_JSON | jq '.es_ssl = true')
       yq w -i /opt/elastalert-server/config/elastalert.yaml "use_ssl" "True"
+      yq w -i /opt/elastalert/config.yaml "use_ssl" "True"
       if [[ "$ELASTALERT_ES_CLIENT_VERIFY_CA" = 'yes' ]]; then
         echo "Turning on ES service TLS validation"
         yq w -i /opt/elastalert-server/config/elastalert.yaml "verify_certs" "True"
+        yq w -i /opt/elastalert/config.yaml "verify_certs" "True"
       else
         echo "Turning off ES service TLS validation"
         yq w -i /opt/elastalert-server/config/elastalert.yaml "verify_certs" "False"
+        yq w -i /opt/elastalert/config.yaml "verify_certs" "False"
       fi
       # es_ca_certs
       echo "Using CA certs from /opt/elastalert-server/client-chain.pem"
       ELASTALERT_CONFIG_JSON=$(echo $ELASTALERT_CONFIG_JSON | jq '.es_ca_certs = "/opt/elastalert-server/client-chain.pem"')
       yq w -i /opt/elastalert-server/config/elastalert.yaml "es_ca_certs" "/opt/elastalert-server/client-chain.pem"
+      yq w -i /opt/elastalert/config.yaml "es_ca_certs" "/opt/elastalert-server/client-chain.pem"
       # es_client_cert
       echo "Using client cert from /opt/elastalert-server/client-cert.pem"
       ELASTALERT_CONFIG_JSON=$(echo $ELASTALERT_CONFIG_JSON | jq '.es_client_cert = "/opt/elastalert-server/client-cert.pem"')
       yq w -i /opt/elastalert-server/config/elastalert.yaml "es_client_cert" "/opt/elastalert-server/client-cert.pem"
+      yq w -i /opt/elastalert/config.yaml "es_client_cert" "/opt/elastalert-server/client-cert.pem"
       # es_client_key
       echo "Using client key from /opt/elastalert-server/client-key.pem"
       ELASTALERT_CONFIG_JSON=$(echo $ELASTALERT_CONFIG_JSON | jq '.es_client_key = "/opt/elastalert-server/client-key.pem"')
       yq w -i /opt/elastalert-server/config/elastalert.yaml "es_client_key" "/opt/elastalert-server/client-key.pem"
+      yq w -i /opt/elastalert/config.yaml "es_client_key" "/opt/elastalert-server/client-key.pem"
     fi
 
     if [[ ! -z $ELASTALERT_ES_HOST ]]; then
       echo "Adding ${ELASTALERT_ES_HOST} to /opt/elastalert/config.yaml"
       ELASTALERT_CONFIG_JSON=$(echo $ELASTALERT_CONFIG_JSON | jq '.es_host = env.ELASTALERT_ES_HOST')
       yq w -i /opt/elastalert-server/config/elastalert.yaml "es_host" "${ELASTALERT_ES_HOST:-localhost}"
+      yq w -i /opt/elastalert/config.yaml "es_host" "${ELASTALERT_ES_HOST:-localhost}"
     fi
 
     echo "Adding ${ELASTALERT_ES_PORT:-9200} to /opt/elastalert/config.yaml"
     if [[ ! -z $ELASTALERT_ES_PORT ]]; then
       ELASTALERT_CONFIG_JSON=$(echo $ELASTALERT_CONFIG_JSON | jq '.es_port = env.ELASTALERT_ES_PORT')
       yq w -i /opt/elastalert-server/config/elastalert.yaml "es_port" "${ELASTALERT_ES_PORT:-9200}"
+      yq w -i /opt/elastalert/config.yaml "es_port" "${ELASTALERT_ES_PORT:-9200}"
     fi
   fi
   # Save the JSON file
@@ -78,8 +86,5 @@ else
   echo "This container has previously been initialized. To cause it to reset, please delete the file /var/ossec/etc/initialized"
 fi
 
-# if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
-#   set -- node "$@"
-# fi
 cd /opt/elastalert-server
 exec npm start
